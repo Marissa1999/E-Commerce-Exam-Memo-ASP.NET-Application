967c5531960f9c37a58d193fe533016dfee488f5
Marissa Gonçalves <marissagoncalves13@gmail.com>
2021-01-05 19:25:41 -0500

    Add project files.

---
+++ MarissaGoncalvesQuestion4/Controllers/HomeController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MarissaGoncalvesQuestion4.Controllers
+{
+    public class HomeController : Controller
+    {
+        Models.PostsEntities database = new Models.PostsEntities();
+
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View(database.Posts);
+        }
+
+        // GET: Home/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Home/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                Models.Post newPost = new Models.Post
+                {
+                    author = collection["author"],
+                    message = collection["message"]
+                };
+
+                database.Posts.Add(newPost);
+                database.SaveChanges();
+              
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Home/Edit/5
+        public ActionResult Edit(int id)
+        {
+            Models.Post thePost = database.Posts.SingleOrDefault(p => p.post_id == id);
+            return View(thePost);
+        }
+
+        // POST: Home/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                Models.Post thePost = database.Posts.SingleOrDefault(p => p.post_id == id);
+
+                thePost.author = collection["author"];
+                thePost.message = collection["message"];
+
+                database.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Home/Delete/5
+        public ActionResult Delete(int id)
+        {
+            Models.Post thePost = database.Posts.SingleOrDefault(p => p.post_id == id);
+            return View(thePost);
+        }
+
+        // POST: Home/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                Models.Post thePost = database.Posts.SingleOrDefault(p => p.post_id == id);
+                database.Posts.Remove(thePost);
+                database.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MarissaGoncalvesQuestion4/Models/Validation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MarissaGoncalvesQuestion4.Models
+{
+    
+    [MetadataType(typeof(Post_Validation))]
+    public partial class Post
+    {
+
+    }
+
+    public class Post_Validation
+    {
+        [Display (Name = "Message")]
+        [Required(ErrorMessage = "Please enter a message for the posts list.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Please enter a message between 2-50 characters.")]
+        public string message;
+
+        [Display(Name = "Author")]
+        [Required (ErrorMessage = "Please enter an author for the posts lists.")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Please enter an author between 2-50 characters.")]
+        public string author;
+    }
+
+
+}
